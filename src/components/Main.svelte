59ace37d17475e0613cbcb9fdc66f30d2408dd2d
muscleblaster <wavareaditya06@gmail.com>
2025-03-19 15:01:50 +0530

    Update Main.svelte

--- src/components/Main.svelte
+++ src/components/Main.svelte
@@ -6,19 +6,20 @@
             name: "Aditya Store",
             icon: "fa-solid fa-cart-shoppig",
             description:
-                "Smoljames Store is a a merchanising store created with Next.js, Commerce.js & Stripe! Commerce.js is a CMS for managing project and Stripe is used for all transaction handling.",
+                " Will be uploading major projects i have done over the year ",
         },
         {
-            name: "Utlimate Todos",
+            name: " Begining projects ",
             icon: "fa-solid fa-list-check",
             description:
-                "Test for model robustness with customized test profiles and receive exhaustive reporting on the performance and potential vulnerabilities of your model.",
+                "  ",
         },
         {
-            name: "Pokédex",
+            name: " Fitness / sports nutitionist ",
             icon: "fa-solid fa-diagram-project",
             description:
-                "Augment your datasets with our filters and dataset manipulations to ensure your models are trained on the highest quality datasets (coming soon).",
+                "Worked a free-lancer  trainer/coach , trained more than 100  clients both offline and oline training. "
+                
         },
     ];
 
